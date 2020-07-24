35c5957299c02bc1ab5014ecfc2d0bda80c0f69f
Hayden Linfoot <hayden.linfoot@gmail.com>
2020-07-23 20:56:48 -0500

    start function

--- index.js
+++ index.js
@@ -25,7 +25,24 @@ connection.connect(function(err) {
 ​
 // function which prompts the user for what action they should take
 function start() {
-​
+​ inquirer
+.prompt({
+  name: "postOrBid",
+  type: "list",
+  message: "Would you like to [POST] an auction or [BID] on an auction?",
+  choices: ["POST", "BID", "EXIT"]
+})
+.then(function(answer) {
+  // based on their answer, either call the bid or the post functions
+  if (answer.postOrBid === "POST") {
+    postAuction();
+  }
+  else if(answer.postOrBid === "BID") {
+    bidAuction();
+  } else{
+    connection.end();
+  }
+});
 }
 ​
 // function to handle posting new items up for auction
